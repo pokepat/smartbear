31a5569a0e43c0f208bd7fcba169d03211021e3e
Patrick <69877596+pokepat@users.noreply.github.com>
2025-07-03 16:23:14 +0530

    Initial Commit

---
+++ src/main/java/com/smartbear/enums/EnumMinuteTens.java
@@ -0,0 +1,22 @@
+package com.smartbear.enums;
+
+enum EnumMinuteTens {
+	TWENTY(20, "twenty"),
+	THIRTY(30, "thirty"),
+	FORTY(40, "forty"),
+	FIFTY(50, "fifty");
+
+	private final int number;
+	private final String word;
+	
+	EnumMinuteTens(int number, String word) {
+		this.number = number;
+		this.word = word;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/smartbear/enums/EnumMinuteUnits.java
@@ -0,0 +1,33 @@
+package com.smartbear.enums;
+
+public enum EnumMinuteUnits {
+
+	ZERO(0, "zero"),
+	ONE(1, "one"),
+	TWO(2, "two"),
+	THREE(3, "three"),
+	FOUR(4, "four"),
+	FIVE(5, "five"),
+	SIX(6, "six"),
+	SEVEN(7, "seven"),
+	EIGHT(8, "eight"),
+	NINE(9, "nine"),
+	TEN(10, "ten"),
+	ELEVEN(11, "eleven"),
+	TWELVE(12, "twelve"),
+	THIRTEEN(13, "thirteen"),
+	FOURTEEN(14, "fourteen"),
+	FIFTEEN(15, "fifteen"),
+	SIXTEEN(16, "sixteen"),
+	SEVENTEEN(17, "seventeen"),
+	EIGHTEEN(18, "eighteen"),
+	NINETEEN(19, "nineteen");
+	
+	private final int number;
+	private final String word;
+	
+	EnumMinuteUnits(int number, String word) {
+		this.number = number;
+		this.word = word;
+	}
+}
